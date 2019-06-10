25ed874f87a106dfca68bcc482e85d322fe34183
Vashu Karn <jaykarvashu@gmail.com>
2019-06-10 15:48:59 +0545

    Front end main page design

--- read.php
+++ read.php
@@ -1,1 +1,208 @@
-<!-- this is test -->
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hostel Front</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+	<!-- Navigation Bar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="read.php"><img src="logo.png" height="62" width="62"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="read.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Testimonials</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Facilities</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Career</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact Us</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Additionals</a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Rooms</a>
+          <a class="dropdown-item" href="#">24 hr electricity</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Wireless Fidelity</a>
+        </div>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+	<!-- Slider -->
+
+<div id="slides" class="carousel slide" data-ride="carousel" >
+<ul class="carousel-indicators">
+	<li data-target="#slides" data-slide-to="0" class="active"></li>
+	<li data-target="#slides" data-slide-to="1"></li>
+	<li data-target="#slides" data-slide-to="2"></li>
+	<li data-target="#slides" data-slide-to="3"></li>
+</ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="hostel1.jpg" alt="First slide" style="height: 400px;">
+      <div class="carousel-caption">
+      	<h1 class="display-2">Midnight Memories</h1>
+      	<h3>Complete Hostel Solution</h3>
+      	<button type="button" class="btn btn-outline-light btn-lg">REGISTER</button>
+      	<button type="button" class="btn btn-primary btn-lg">Log In!</button>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="hostel2.jpg" alt="Second slide" style="height: 400px;">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="hostel3.jpg" alt="Third slide" style="height: 400px;">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="hostel4.jpg" alt="Second slide" style="height: 400px;">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#slides" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#slides" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
+
+<!-- Jumbotron -->
+<div class="container-fluid">
+<div class="row jumbotron">
+	<div class="col-xs-12 col-sm-12 col-md-9 col-lg-9 col-xl-10"><p class="lead">It is rightly said that a hostel is a home away from home. For hostellers, their rooms become their small world. Necessary measures are taken to ensure the cleanliness and safety of all the hostels. The hostel service is student friendly and gives them a feeling of being at home.</p>
+	</div>
+	<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-2">
+		<button type="button" class="btn btn-outline-secondary btn-lg"> Hell Yeah!! </button>
+	</div>
+		
+</div>
+</div>
+<!-- Welcome Section -->
+<div class="container-fluid padding">
+<div class="row welcome text-center">
+	<div class="col-12">
+		<h1 class="display-4">Living with an ease.</h1>
+	</div>	
+	<hr>
+	<div class="col-12">
+		<p class="lead"> Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Someti.... </p>
+	</div>
+</div>
+</div>
+<!-- Three Column Section -->
+<br>
+<div class="container-fluid padding">
+<div class="row text-center padding">
+	<div class="col-xs-12 col-sm-6 col-md-3">
+		<img src="power-back-up.png">
+		<h3>Power Backup</h3>
+		<p> 24X7 Power back-up facility is available in every hostel and round-the-clock security arrangement is in place in all the hostels. </p>
+
+	</div>
+	<div class="col-xs-12 col-sm-6 col-md-3">
+		<img src="air-conditioned.png">
+		<h3>Air Conditioned</h3>
+		<p> Air-conditioned and non air-conditioned rooms are available for both boys and girls. </p>
+
+	</div>
+	<div class="col-xs-12 col-sm-6 col-md-3">
+		<img src="rooms.png">
+		<h3>Rooms</h3>
+		<p> Spacious rooms with necessary furniture for taking rest/sleeping as well as studying are provided to the students. </p>
+
+	</div>
+	<div class="col-xs-12 col-sm-6 col-md-3">
+		<img src="discipline.png">
+		<h3>Discipline</h3>
+		<p> The students are required to maintain discipline at all times and are not allowed to leave the hostel without prior permission of the competent authority. </p>
+
+	</div>
+</div>
+<hr class="my-4"> 
+</div>
+<!-- Footer -->
+<footer>
+<div class="container-fluid padding">
+<div class="row text-center">
+	<div class="col-md-4">
+		<img src="logo.png" height="72" width="72">
+		<hr class="light">
+		<p>98784561512</p>
+		<p>Jaykarvashu@gmail.com</p>
+		<p>Pepsicola, Kathmandu</p>
+		<p>Kathmandu, 442200</p>
+	</div>
+	<div class="col-md-4">
+		<hr class="light">
+		<h5>Yeah!!</h5>
+		<hr class="light">
+		<p>123</p>
+		<p>456</p>
+		<p>789</p>
+		<p>789456123</p>
+	</div>
+	<div class="col-md-4">
+		<hr class="light">
+		<h5>Yeah!!</h5>
+		<hr class="light">
+		<p>123</p>
+		<p>456</p>
+		<p>789</p>
+		<p>78945123</p>
+	</div>
+	<div class="col-12">
+		<hr class="light">
+		<h5>&copy; Hostelmanagement.com</h5>
+	</div>
+</div>
+</div>
+</footer>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+	
+</html>
